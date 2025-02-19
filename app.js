2ad54bb017702a6e42fea4d43eb3314a65389f71
Bryce King <150635135+bryyc3@users.noreply.github.com>
2025-02-19 20:23:55 +0000

    Receive email info

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
 
 const app = express();
 const PORT = 5000;
 
 app.use(express.json());
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true
+}));
+
+app.use(express.json());
 
 app.post('/', (req, res)=>{
     console.log(req.body);
-})
+});
 
 
 
