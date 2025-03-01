21099ae5fdcd42cfbbdd15d7ec0b97a9216325f1
Bryce King <bryceeking5@gmail.com>
2025-03-01 15:21:12 -0500

    new dist folder

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ const transporter = nodemailer.createTransport({
 })
 
 app.use(express.json());
+app.use(express.static('dist'));
 
 app.post('/api/email', (req, res)=>{
     const emailDetails = req.body;
