e35e601e92aedcc0ca084a63e41a5d33b768a024
Bryce King <bryceeking5@gmail.com>
2025-02-28 21:29:01 -0500

    email sent response

--- app.js
+++ app.js
@@ -1,22 +1,48 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
-import { Email } from './email.js';
-
+import nodemailer from 'nodemailer';
+dotenv.config();
 
 const app = express();
-const PORT = 5050;
+const PORT = process.env.PORT || 5050;
+
+const transporter = nodemailer.createTransport({
+    host: process.env.EMAIL_SERVICE,
+    port: process.env.EMAIL_PORT,
+    secure:true,
+    auth: {
+        user: process.env.EMAIL,
+        pass: process.env.EMAIL_PASSWORD
+    }
+})
+
 
 app.use(express.json());
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: process.env.ORIGIN_ADDRESS,
     credentials: true
 }));
 
 app.use(express.json());
 
 app.post('/', (req, res)=>{
-    Email(req.body)
+    const emailDetails = req.body;
+    const mailOptions = {
+        from: emailDetails.email,
+        to: process.env.EMAIL,
+        subject: `${emailDetails.category} Consultation From ${emailDetails.email}`,
+        text: emailDetails.description
+    };
+
+    transporter.sendMail(mailOptions, (err, info)=>{
+        if(err){
+            res.send({emailSent: false});
+        }
+        else{
+            res.send({emailSent: true});
+        }
+    })
 });
 
 
